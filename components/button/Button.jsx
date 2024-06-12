ffcdad0997da5ecbdc35b796773109bf318b6db5
Shmuelt <tufas29@gmail.com>
2024-06-12 16:42:55 +0300

    starter

--- components/button/Button.jsx
+++ components/button/Button.jsx
@@ -1,6 +1,8 @@
 import style from "./button.module.css";
 
 const Button = ({ location, active }) => {
+  const handleClick = () => {};
+
   return (
     <button
       className={`${style.button} ${active ? style.active : ""}`}
